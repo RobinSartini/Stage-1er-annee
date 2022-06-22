112d90da3ca944361545956b4110c798488032fc
RobinSartini <robinsartini5@gmail.com>
2022-06-22 03:03:53 +0200

    fin de stage 2

--- components/Footer.js
+++ components/Footer.js
@@ -3,77 +3,77 @@ import React from "react";
 const Footer = () => {
     return (
       <a name="f">
-        <footer class="text-slate-50 body-font">
-        <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-          <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">CONTACT</h2>
-              <nav class="list-none mb-10">
+        <footer className="text-slate-50 body-font">
+        <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">CONTACT</h2>
+              <nav className="list-none mb-10">
                 <li>
-                  <a class="text-slate-50 hover:text-slate-50">Kheireddine.safi@gmail.com</a>
+                  <a className="text-slate-50 hover:text-slate-50">Kheireddine.safi@gmail.com</a>
                 </li>
               </nav>
             </div>
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">RESEAUX</h2>
-              <nav class="list-none mb-10">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">RESEAUX</h2>
+              <nav className="list-none mb-10">
                 <li>
-                  <a href="https://twitter.com/Kekexflex" class="text-slate-50 hover:text-slate-50">Twitter</a>
+                  <a href="https://twitter.com/Kekexflex" className="text-slate-50 hover:text-slate-50">Twitter</a>
                 </li>
                 <li>
-                  <a href="https://www.instagram.com/kyuphotography__/" class="text-slate-50 hover:text-slate-50">Instagram</a>
+                  <a href="https://www.instagram.com/kyuphotography__/" className="text-slate-50 hover:text-slate-50">Instagram</a>
                 </li>
                 <li>
-                  <a href="https://m.facebook.com/kheireddinesetiff" class="text-slate-50 hover:text-slate-50">FaceBook</a>
+                  <a href="https://m.facebook.com/kheireddinesetiff" className="text-slate-50 hover:text-slate-50">FaceBook</a>
                 </li>
               </nav>
             </div>
 
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">PROFESSIONEL</h2>
-              <nav class="list-none mb-10">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">PROFESSIONEL</h2>
+              <nav className="list-none mb-10">
                 <li>
-                  <a href="https://www.linkedin.com/in/kheireddine-safi-47b1ba1b2/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr"  class="text-slate-50 hover:text-slate-50">Linkedin</a>
+                  <a href="https://www.linkedin.com/in/kheireddine-safi-47b1ba1b2/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr"  className="text-slate-50 hover:text-slate-50">Linkedin</a>
                 </li>
                 <li>
-                  <a  class="text-slate-50 hover:text-slate-50">Kheireddine.safi@gmail.com</a>
+                  <a  className="text-slate-50 hover:text-slate-50">Kheireddine.safi@gmail.com</a>
                 </li>
 
               </nav>
             </div>
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">LOCALISATION</h2>
-              <nav class="list-none mb-10">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-slate-50 tracking-widest text-sm mb-3">LOCALISATION</h2>
+              <nav className="list-none mb-10">
                 <li>
-                  <a class="text-slate-50 hover:text-slate-50">Lyon</a>
+                  <a className="text-slate-50 hover:text-slate-50">Lyon</a>
                 </li>
               </nav>
             </div>
           </div>
         </div>
 
-        <div class="bg-slate-50">
-          <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-             <a href="https://twitter.com/Kekexflex" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@Kekexflex</a>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a class="text-slate-50">
-                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+        <div className="bg-slate-50">
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+             <a href="https://twitter.com/Kekexflex" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@Kekexflex</a>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+              <a className="text-slate-50">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
-              <a class="ml-3 text-slate-50">
-                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+              <a className="ml-3 text-slate-50">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
-              <a class="ml-3 text-slate-50">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+              <a className="ml-3 text-slate-50">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
-              <a class="ml-3 text-slate-50">
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+              <a className="ml-3 text-slate-50">
+                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
                   <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,21 +4,21 @@ import React from "react"
 const Navigation = () => {
     return (
 
-<div class="bg-neutral-900">
-  <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-    <div class="relative flex items-center justify-between">
-      <a href="/" aria-label="KuyPhotoGraphy" title="KuyPhotoGraphy" class="inline-flex items-center">
-        <span class="ml-2 text-xl font-bold tracking-wide text-slate-50 uppercase">KuyPhotoGraphy</span>
+<div className="bg-neutral-900">
+  <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="relative flex items-center justify-between">
+      <a href="/" aria-label="KuyPhotoGraphy" title="KuyPhotoGraphy" className="inline-flex items-center">
+        <span className="ml-2 text-xl font-bold tracking-wide text-slate-50 uppercase">KuyPhotoGraphy</span>
       </a>
-      <ul class="flex items-center hidden space-x-8 lg:flex">
-        <li><a href="/" aria-label="Acceuil" title="Acceuil" class="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Acceuil</a></li>
-        <li><a href="#o" aria-label="Mon Portfolio" title="Mon Portfolio" class="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">PortFolio</a></li>
-        <li><a href="#t" aria-label="Tarifs" title="Tarifs" class="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Tarifs</a></li>
-        <li><a href="#f" aria-label="Contact" title="Contact" class="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Contact</a></li>
+      <ul className="flex items-center hidden space-x-8 lg:flex">
+        <li><a href="/" aria-label="Acceuil" title="Acceuil" className="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Acceuil</a></li>
+        <li><a href="#o" aria-label="Mon Portfolio" title="Mon Portfolio" className="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">PortFolio</a></li>
+        <li><a href="#t" aria-label="Tarifs" title="Tarifs" className="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Tarifs</a></li>
+        <li><a href="#f" aria-label="Contact" title="Contact" className="font-medium tracking-wide text-slate-50 transition-colors duration-200 hover:text-teal-accent-400">Contact</a></li>
       </ul>
-      <div class="lg:hidden">
-        <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline">
-          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+      <div className="lg:hidden">
+        <button aria-label="Open Menu" title="Open Menu" className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline">
+          <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
             <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
             <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
             <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
