1d9c39b8b53387e77e360f49e36302e6356fa7d2
RobinSartini <robinsartini5@gmail.com>
2022-06-22 03:47:48 +0200

    fin de stage 6

--- components/galery.js
+++ components/galery.js
@@ -29,65 +29,65 @@ const Gallery = () => {
   <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
   <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
 
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image1} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image1} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
 
 
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image2} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image2} width={"310"} height={"300"} alt="PortFolio"  className="object-cover w-full h-64 rounded"/></a>
 
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image3} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image3} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
    
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image4} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image4} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
    
   
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image5} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image5} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
     
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image6} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image6} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
  
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image7} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image7} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image8} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image8} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image9} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image9} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
 
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image10} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image10} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
    
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image11} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image11} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image12} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image12} width={"310"} height={"300"} alt="PortFolio"  className="object-cover w-full h-64 rounded"/></a>
  
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image13} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image13} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
  
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image14} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image14} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
  
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image15} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image15} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image16} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image16} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
 
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image17} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image17} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image18} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image18} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
 
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image19} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image19} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
 
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image20} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image20} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
   
     
-      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image21} width={"310"} height={"300"} className="object-cover w-full h-64 rounded"/></a>
+      <a href="https://www.instagram.com/kyuphotography__/" aria-label="Article"><Image  src={image21} width={"310"} height={"300"} alt="PortFolio" className="object-cover w-full h-64 rounded"/></a>
       </div>
       </div>
 </a>
